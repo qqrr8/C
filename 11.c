3e9e46c24bb3369219337e361cc8d6f66b3da819
qqrr8 <146338628+qqrr8@users.noreply.github.com>
2023-12-02 11:04:11 +0300

    Update 11.c

--- 11.c
+++ 11.c
@@ -8,20 +8,24 @@
 void main()
 {
     setlocale(LC_ALL, "RUS");
-	float A[N];
+    float A[N];
     float temp;
     for (int i = 0; i < 10; i++)
     {
         printf("a[%d] = ", i + 1);
         scanf("%f", &temp);
-        A[i] = temp;    
+        A[i] = temp;
     }
     printf(" индекс  | исходное значение | новое значение");
     for (int i = 0; i <= 9; i++)
     {
-        printf("\n A[%d]    | %.0f                 | %.0f", i, A[i], pow(A[i],2));
+        printf("\n A[%d]    | %.0f                 | %.0f", i, A[i], pow(A[i], 2));
     }
-    float b = (A[0] + A[1] + A[2] + A[3] + A[4] + A[5] + A[6] + A[7] + A[8] + A[9]) / 10;
-    printf("\nСреднее арифметическое:%f\n", b);
+    float sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += A[i];
+    }
+    printf("\nСреднее арифметическое:%f\n", sum/10);
     system("pause");
 }
