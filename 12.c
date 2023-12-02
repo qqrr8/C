27ab72fb0104b93a644b72c09adcfe3510fe002d
qqrr8 <146338628+qqrr8@users.noreply.github.com>
2023-12-02 08:40:08 +0300

    Update 12.c

--- 12.c
+++ 12.c
@@ -53,3 +53,21 @@ x--;
 printf("По даресу %p хранится *ptr=%d\n", pa, *pa);
 }
 
+//ex 5
+void main(void)
+{
+	int x = 0x12345678;
+	int* prt;
+	unsigned char* a1, * a2, * a3, * a4;
+	unsigned char* a;
+
+	prt = &x;
+	a = (unsigned char*)prt;
+
+	a1 = a + 3;
+	a2 = a + 2;
+	a3 = a + 1;
+	a4 = a;
+
+	printf("%.2x\n%.2x\n%.2x\n%.2x\n", *a1, *a2, *a3, *a4);
+}
