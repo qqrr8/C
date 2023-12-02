57007dd1494dfeb376303d7a40338474728cb55b
qqrr8 <146338628+qqrr8@users.noreply.github.com>
2023-12-02 08:33:04 +0300

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <locale.h>
-#define _CRT_SECURE_NO_WARNINGS
 
 //ex 1
 int main()
@@ -54,26 +53,3 @@ x--;
 printf("По даресу %p хранится *ptr=%d\n", pa, *pa);
 }
 
-//ex 4
-int main()
-{
-setlocale(LC_ALL, "RUS");
-
-float array[10] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.1 };
-
-float* ptr_a;
-ptr_a = array;
-printf("По даресу %p хранится *ptr=%f\n", ptr_a, *ptr_a);
-ptr_a++;
-printf("По даресу %p хранится *ptr=%f\n", ptr_a, *ptr_a);
-ptr_a++;
-printf("По даресу %p хранится *ptr=%f\n", ptr_a, *ptr_a);
-ptr_a--;
-printf("По даресу %p хранится *ptr=%f\n", ptr_a, *ptr_a);
-ptr_a += 2;
-printf("По даресу %p хранится *ptr=%f\n", ptr_a, *ptr_a);
-ptr_a += 2;
-printf("По даресу %p хранится *ptr=%f\n", ptr_a, *ptr_a);
-ptr_a -= 2;
-printf("По даресу %p хранится *ptr=%f\n", ptr_a, *ptr_a);
-}
