e1b6baa7de5678e036995c1166c8efd55fb3d764
qqrr8 <146338628+qqrr8@users.noreply.github.com>
2023-12-21 17:20:34 +0300

    Update f1.c

--- f1.c
+++ f1.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include <math.h>
 #define _USE_MATH_DEFINES
-void main()
+
+float function(float x)
 {
-	setlocale(LC_ALL, "RUS");
-	printf("Программа для вычесления значения функции по заданному значению x\n");
+	return x * log(pow(x, 4)) - tan(sqrt(1 + pow(x, 2)));
+}
+
+int main()
+{
 	float x;
-	printf("Введите значение x для функции: ");
+	setlocale(LC_ALL, "RUS");
+	printf("Программа для вычесления значения функции по заданному значению 'x'\n");
+	printf("Введите значение 'x' для функции: ");
 	scanf("%f", &x);
-	float f = x * log(pow(x, 4)) - tan(sqrt(1 + pow(x, 2)));
 	printf("Вы ввели x = %.2f\n", x);
-	printf("Ответ: %.2f\n", f);
+	printf("Ответ: %.2f\n", function(x));
 	system("pause");
 }
